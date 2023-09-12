c625190363d5b5953239f4611578e0d48b3b67a2
l00ke3 <71680654+l00ke3@users.noreply.github.com>
2023-09-12 21:33:41 +0100

    Update pre-install.sh

--- start_up/pre-install.sh
+++ start_up/pre-install.sh
@@ -7,5 +7,6 @@ echo "version :"
 mkdir -p /root/lia_pro
 
 pip3 install ffmpeg requests TikTokLive
+git clone https://github.com/l00ke3/lia_pro.git /root/lia_pro
 
 echo "---------------------------> PRE INSTALL  DONNE <--------------------------------------"
