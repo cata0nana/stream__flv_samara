5602c2be1f51e6fcf07a1e1c0687175c946ff70a
l00ke3 <71680654+l00ke3@users.noreply.github.com>
2023-09-12 21:15:56 +0100

    Update pre-install.sh

--- start_up/pre-install.sh
+++ start_up/pre-install.sh
@@ -3,19 +3,9 @@
 echo "---------------------------PRE INSTALL --------------------------------------"
 
 echo "version :"
-# pwd
-# ls
 
-# lsb_release -a
-curl -LOks https://mega.nz/linux/repo/Debian_11/amd64/megasync-Debian_11_amd64.deb 
-curl -LOks https://mega.nz/linux/repo/Debian_11/amd64/megacmd-Debian_11_amd64.deb
+mkdir -p /root/lia_pro
 
-
-cp *.deb /tmp/ 
-ls /tmp/
-apt install /tmp/megasync-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
-
-apt install /tmp/megacmd-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
-rm /tmp/*.deb
+pip3 install ffmpeg requests TikTokLive
 
 echo "---------------------------> PRE INSTALL  DONNE <--------------------------------------"
